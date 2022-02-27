51c7d9f6ffbb15c761124e521366a54fe0de1247
Yuri Gbur <k.gbur@tu-berlin.de>
2022-02-27 13:20:50 -0800

    added self-contained dockerfiles for mvfst, neqo and nginx

---
+++ servers/mvfst/run_endpoint.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Extra debugging ?
+set -x
+set -o nounset
+
+DRAFT=29
+HQ_CLI=/proxygen/_build/proxygen/bin/hq
+PORT=12345
+LOGLEVEL=2
+
+# Unless noted otherwise, test cases use HTTP/0.9 for file transfers.
+PROTOCOL="h3-29"
+HTTPVERSION="3.0"
+
+# Default enormous flow control.
+
+CONN_FLOW_CONTROL="107374182"
+STREAM_FLOW_CONTROL="107374182"
+INVOCATIONS=$(echo ${REQUESTS} | tr " " "\n" | awk -F '/' '{ print "/" $4 }' | paste -sd',')
+EARLYDATA="false"
+PSK_FILE="" # in memory psk
+
+echo "mvfst"
+echo "Running QUIC server on 0.0.0.0:${PORT}"
+    ${HQ_CLI} \
+        --mode=server \
+	--cert=/mnt/certs/mvfst.pem \
+	--key=/mnt/certs/mvfst.key \
+        --port=${PORT} \
+	--httpversion=${HTTPVERSION} \
+        --h2port=${PORT} \
+        --static_root=/www \
+        --logdir=/logs \
+	--qlogger_path=/logs \
+        --host=0.0.0.0 \
+        --congestion=bbr \
+        --pacing=true \
+        --v=${LOGLEVEL} 2>&1 | tee /logs/server.log
---
+++ servers/neqo/run_endpoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ex
+
+export PATH="${PATH}:/neqo/bin"
+
+DB=/neqo/db
+CERT=cert
+P12CERT=$(mktemp)
+mkdir -p "$DB"
+certutil -N -d "sql:$DB" --empty-password
+openssl pkcs12 -export -nodes -in /mnt/certs/ca_quicly.pem -inkey /mnt/certs/ca.key \
+	-name "$CERT" -passout pass: -out "$P12CERT"
+pk12util -d "sql:$DB" -i "$P12CERT" -W ''
+certutil -L -d "sql:$DB" -n "$CERT"
+RUST_LOG=info RUST_BACKTRACE=1 neqo-server --cc cubic -o -d "$DB" -k "$CERT" 0.0.0.0:12345
