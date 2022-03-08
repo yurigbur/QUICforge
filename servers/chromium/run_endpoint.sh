0639f0e0165ed6440e7e4622ab753b93768214f0
Yuri Gbur <k.gbur@tu-berlin.de>
2022-03-08 13:41:53 -0800

    updated docker for chromium quiche. Still WIP

--- servers/chromium/run_endpoint.sh
+++ servers/chromium/run_endpoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-#Proxy version, needs backend server
-#TODO docker compose for quic-server and http backed
-/chromium/out/Debug/quic_server --quic_mode=proxy --quic_proxy_backend_url=http://192.168.79.136:9090 --certificate_file=/home/masterserver/quic/certs/out/leaf_cert.pem --key_file=/home/masterserver/quic/certs/out/leaf_cert.pkcs8 --port=12345
+cd /www && python3 -m http.server &
+
+/chromium/quic_server \
+  --quic_mode=proxy \
+  --quic_proxy_backend_url=http://localhost:8000 \
+  --certificate_file=/mnt/certs/leaf_cert.pem \
+  --key_file=/mnt/certs/leaf_cert.pkcs8 \
+  --port=12345
