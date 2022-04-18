b3cba74d76dea7d900950e67505b8fa9577ee253
Yuri Gbur <k.gbur@tu-berlin.de>
2022-04-18 15:33:53 -0700

    updated kwik and lsquic

--- servers/lsquic/run_endpoint.sh
+++ servers/lsquic/run_endpoint.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+echo "Lsquic version:"
+cat /git_version.txt
+echo "Starting server..."
+
 http_server -c www.example.com,/mnt/certs/ca.pem,/mnt/certs/ca.key -r /www -G /mnt/keys -L debug
