9f31210197d16acff9afc00c175a6b57b9c544db
Yuri Gbur <k.gbur@tu-berlin.de>
2022-05-01 15:14:01 -0700

    adjusted quicly settings

--- servers/quicly/run_endpoint.sh
+++ servers/quicly/run_endpoint.sh
@@ -4,4 +4,4 @@
 echo "Quicly version:"
 cat /git_version.txt
 echo "Starting server ..."
-/quicly/cli -k /mnt/certs/ca.key -c /mnt/certs/ca_quicly.pem -v 0.0.0.0 12345
+/quicly/cli -k /mnt/certs/ca.key -c /mnt/certs/ca_quicly.pem -a h3 -v 0.0.0.0 12345 -l /mnt/keys/quicly.log -e /dev/stdout
